25e587cc4ee75d16d51102c6c91c2ee9ad6979fa
Gustavo <gustavokkg2@hotmail.com>
2022-01-28 14:08:34 -0300

    Adiciona StatusCode

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response, NextFunction } from "express";
-
+import { StatusCodes } from 'http-status-codes';
 // get /users
 // get /users/:uuid
 // post /users
@@ -10,13 +10,13 @@ const usersRoute = Router();
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
     const users = [{ userName: 'Gustavo' }];
-    res.status(200).send( users );
+    res.status(StatusCodes.OK).send( users );
 
 });
 
 usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
-    res.status(200).send({ uuid });
+    res.status(StatusCodes.OK).send({ uuid });
 })
 
 
