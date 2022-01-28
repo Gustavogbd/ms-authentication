ef32a28a55132da7a47f8abeb236faa426ee9d67
Gustavo <gustavokkg2@hotmail.com>
2022-01-28 13:55:34 -0300

    Implementando rota por id de usuário

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -14,4 +14,10 @@ usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
 
 });
 
+usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    res.status(200).send({ uuid });
+})
+
+
 export default usersRoute;
