fca0241cb019e6b77a2ad72d8007b8d3276fe899
Gustavo <gustavokkg2@hotmail.com>
2022-01-28 14:31:26 -0300

    Desenvolve método PUT e DELETE

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -22,7 +22,21 @@ usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, n
 usersRoute.post('/users', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
     const newUser = req.body;
     res.status(StatusCodes.CREATED).send(newUser);
-} )
+});
+
+usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    const modifiedUser = req.body;
+
+    modifiedUser.uuid = uuid;
+
+    res.status(StatusCodes.OK).send({ modifiedUser });
+});
+
+usersRoute.delete('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    res.status(StatusCodes.OK).send({ uuid });
+});
 
 
 export default usersRoute;
