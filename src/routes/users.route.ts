872ba5641e30b48d979894cb2d9ebf344f0f6350
Gustavo <gustavokkg2@hotmail.com>
2022-01-28 13:18:56 -0300

    Introdução ao desenvolvimento do CRUD de usuários

---
+++ src/routes/users.route.ts
@@ -0,0 +1,17 @@
+import { Router, Request, Response, NextFunction } from "express";
+
+// get /users
+// get /users/:uuid
+// post /users
+// put /users/:uuid
+// delete /users/:uuid
+
+const usersRoute = Router();
+
+usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
+    const users = [{ userName: 'Gustavo' }];
+    res.status(200).send( users );
+
+});
+
+export default usersRoute;
