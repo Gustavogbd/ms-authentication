aadb2b76cf370cc0f0387f3e868733588dd5a811
Gustavo <gustavokkg2@hotmail.com>
2022-01-29 11:34:03 -0300

    Criação de Pool de conexões

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response, NextFunction } from "express";
 import { StatusCodes } from 'http-status-codes';
+import userRepository from "../repositories/user.repository";
 // get /users
 // get /users/:uuid
 // post /users
@@ -8,8 +9,8 @@ import { StatusCodes } from 'http-status-codes';
 
 const usersRoute = Router();
 
-usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
-    const users = [{ userName: 'Gustavo' }];
+usersRoute.get('/users', async (req: Request, res: Response, next: NextFunction) => {
+    const users = await userRepository.findAllUsers();
     res.status(StatusCodes.OK).send(users);
 
 });
